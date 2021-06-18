1a4a65ee87fd14f612699f6d4df192c81a9cdd59
Zeena Zhang <zhangyujie258@gmail.com>
2021-06-18 13:47:47 +1000

    w3

---
+++ set3/in_lec.py
@@ -0,0 +1,12 @@
+for i in [1, 2, 3]:
+    print(i * 2)
+
+print(2 in [2, 3, 4])
+print(8 in [2, 3, 4])
+print("2" in [2, 3, 4])
+print({} in [2, 3, 4])
+print("car" in "carpet")
+
+print(7 in {"key": 7, "otherKey": 5})
+print("key" in {"key": 7, "otherKey": 5})
+print({"key": 7, "otherkey": 5})
